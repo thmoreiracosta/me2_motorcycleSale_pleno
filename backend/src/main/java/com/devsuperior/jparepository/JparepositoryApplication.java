0d76a31596227202a1fd21a59a3fabde965c2ca5
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-14 22:42:46 -0300

    init API backend

---
+++ backend/src/main/java/com/devsuperior/jparepository/JparepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jparepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparepositoryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/jparepository/JparepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
